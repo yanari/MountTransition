3ef78c825e49a4f87ab9ded458fb4296cf9d3aeb
Marcelle <yanarimy@gmail.com>
2019-05-30 12:11:55 -0300

    changed readme & names, example & preset added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import MountTransition from './MountTransition/index';
 
 function App() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MountTransition show = {open} preset = "fadeInOut">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </MountTransition>
+      <button onClick = {() => setOpen(!open)}>a</button>
     </div>
   );
 }
--- src/MountTransition/index.js
+++ src/MountTransition/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {animated, Transition} from 'react-spring/renderprops';
 
-function TransitionRenderProps (props) {
+function MountTransition (props) {
   const {
     children,
     className,
-    handleClick,
-    measurementUnit,
-    openHeight,
     preset,
     show,
     tag,
@@ -19,17 +16,16 @@ function TransitionRenderProps (props) {
       atFrom: {opacity: 0},
       atLeave: {opacity: 0},
     },
-    toggle: {
-      atEnter: {height: openHeight + measurementUnit},
-      atFrom: {height: 0 + measurementUnit},
-      atInitial: {height: 0 + measurementUnit},
-      atLeave: {height: 0 + measurementUnit},
+    slideTop: {
+      atEnter: {transform: 'translateY(0%)'},
+      atFrom: {transform: 'translateY(-100%)'},
+      atLeave: {transform: 'translateY(-100%)'},
     },
-    toggleFadeInOut: {
-      atEnter: {height: openHeight + measurementUnit, opacity: 1},
-      atFrom: {height: 0 + measurementUnit, opacity: 0},
-      atInitial: {height: 0 + measurementUnit},
-      atLeave: {height: 0 + measurementUnit, opacity: 0},
+    toggle: {
+      atEnter: {height: '100%'},
+      atFrom: {height: '0%'},
+      atInitial: {height: '0%'},
+      atLeave: {height: '0%'},
     },
   };
   return (
@@ -50,7 +46,7 @@ function TransitionRenderProps (props) {
           } else if (tag === 'figure') {
             return <animated.figure className = {className} style = {style}>{children}</animated.figure>;
           } else {
-            return <animated.div className = {className} onClick = {handleClick} style = {style}>{children}</animated.div>;
+            return <animated.div className = {className} style = {style}>{children}</animated.div>;
           }
         }
         : null)
@@ -59,20 +55,16 @@ function TransitionRenderProps (props) {
   );
 }
 
-TransitionRenderProps.propTypes = {
+MountTransition.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
-  measurementUnit: PropTypes.string,
-  openHeight: PropTypes.number,
   preset: PropTypes.oneOf(['fadeInOut', 'toggle', 'toggleFadeInOut']).isRequired,
   show: PropTypes.bool,
   tag: PropTypes.oneOf(['div', 'figure', 'nav', 'ul']),
 };
 
-TransitionRenderProps.defaultProps = {
-  measurementUnit: '%',
-  openHeight: 100,
+MountTransition.defaultProps = {
   tag: 'div',
 };
 
-export default TransitionRenderProps;
+export default MountTransition;
